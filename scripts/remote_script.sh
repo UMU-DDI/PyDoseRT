7578f398e2eb87f0c853cec8321a2ec450eb8b55
attilasimko <attila.simko@umu.se>
2025-11-04 16:22:31 +0100

    new toml and script

--- scripts/remote_script.sh
+++ scripts/remote_script.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=/cephyr/users/attilas/Alvis/out/%J_error.out
 #SBATCH --output=/cephyr/users/attilas/Alvis/out/%J_output.out
 
-module load virtualenv/20.26.2-GCCcore-13.3.0
+module load virtualenv/20.32.0-GCCcore-14.3.0
 # module --ignore-cache load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 source /cephyr/users/attilas/Alvis/data/attila/autoenv/bin/activate
 
