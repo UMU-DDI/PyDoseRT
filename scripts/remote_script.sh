d665dbe1bc3a14628fbea6413ed6aa1ccc310590
attilasimko <attila.simko@umu.se>
2025-11-04 09:50:20 +0100

    new script

--- scripts/remote_script.sh
+++ scripts/remote_script.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=/cephyr/users/attilas/Alvis/out/%J_output.out
 
 module --ignore-cache load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
-source /cephyr/users/attilas/Alvis/data/newenv/newenv/bin/activate
+source /cephyr/users/attilas/Alvis/data/attila/autoenv/bin/activate
 
 python3 scripts/static_optimize_single_patient.py
 wait
