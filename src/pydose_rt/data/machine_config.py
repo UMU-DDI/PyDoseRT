13a998166647cb99f2d7ad882f219371292390d4
attilasimko <attila.simko@umu.se>
2025-11-06 11:16:39 +0100

    bug

--- src/pydose_rt/data/machine_config.py
+++ src/pydose_rt/data/machine_config.py
@@ -220,7 +220,7 @@ class MachineConfig(BaseSettings):
             end,
             self.number_of_cps,
             endpoint=False,
-        ) % math.pi
+        ) % 2 * math.pi
 
     @computed_field(repr=False)
     @property
