7db49e71ceb85b07c1a251a126b2f7c1ebe5c46c
attilasimko <attila.simko@umu.se>
2025-11-11 11:06:39 +0100

    new bank loss

--- scripts/static_optimize_umea.py
+++ scripts/static_optimize_umea.py
@@ -243,7 +243,7 @@ def compute_mae_loss(dose_pred, dose_true, pred_mus, leafs, pred_jaws, weights, 
     for index, mask in enumerate([masks[0], masks[1], masks[-1]]):
         losses.append(torch.mean(torch.abs((dose_true - dose_pred)[mask > 0])**2))
     jaw_loss = torch.mean(torch.abs(leafs[:, :, 1:, :] - leafs[:, :, :-1, :])**2)
-    bank_loss = torch.mean(torch.abs(leafs[:, 1:, :, :] - leafs[:, 1:, :, :])**2)
+    bank_loss = torch.mean(torch.abs(leafs[:, :, :, 1:] - leafs[:, :, :, 1:])**2)
     losses.append(scale_loss(jaw_loss, weights["leaf_complexity_loss"]))
     losses.append(scale_loss(bank_loss, weights["leaf_reg_loss"]))
 
