7bc5ef202e9a578a5fd43781e741d18e5e1c4914
attilasimko <attila.simko@umu.se>
2026-02-25 10:28:37 +0100

    script cleanup

--- scripts/run_remote.sh
+++
@@ -1,5 +0,0 @@
-for patient_name in P01 P02 P03 P04 P05 P06 P07 P08 P09 P10 P11 P12 P13 P14 P15 P16 P17 P18 P19
-do
-    sbatch scripts/remote_script.sh "$patient_name"
-    sbatch scripts/remote_script.sh "$patient_name"
-done
